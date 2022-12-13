3ecc617a2e0cadbfc4de74bf76467d035fc8d7d6
DarioMarcuzzi <darik95@hotmail.com>
2022-12-13 13:40:37 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from "./componentes/home/Home.jsx"
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./componentes/home/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-        <div>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-          </Routes>
-        </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-    
   );
 }
 
